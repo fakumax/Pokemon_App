d3e2ef82e16ce58f004d9eae052318270875cfd1
Facundo Vergara <fakumax@gmail.com>
2021-08-03 22:26:51 -0300

    Fix Url to deploy

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,7 +1,7 @@
-const POKEMON_LOCAL = 'http://localhost:3001/pokemons';
-const TYPES_LOCAL = 'http://localhost:3001/types';
-const SEARCH_POKEMON = 'http://localhost:3001/pokemons?name=';
-const POKEMON_LOCAL_ID = 'http://localhost:3001/pokemons';
+const POKEMON_LOCAL = '/pokemons';
+const TYPES_LOCAL = '/types';
+const SEARCH_POKEMON = '/pokemons?name=';
+const POKEMON_LOCAL_ID = '/pokemons';
 
 module.exports = {
     POKEMON_LOCAL,
