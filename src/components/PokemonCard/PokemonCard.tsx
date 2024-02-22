c34f7287c2686d4dec8cfb356bc3080b816de04e
Facundo <fakumax@gmail.com>
2024-02-21 23:41:38 -0300

    Create Getpokemon

---
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+const PokemonCard = () => {
+  console.log("listpokemon");
+  return (
+    <div>
+      PokemonCard
+      <div>PokemonCard</div>
+      <div>PokemonCard</div>
+    </div>
+  );
+};
+export { PokemonCard };
